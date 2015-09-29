218a42861dfd037e81227e27f3020ec0d40a89f9
Jong Woo Nam <jwnam0709@gmail.com>
2015-09-29 14:26:08 -0400

    final

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from pattern.web import *
 from jong_miniproject import*
 book_dict = save_text(save_url(1),11)
 print_text(book_dict)
-# a = convert2doc(book_dict)
-# cluster = create_cluster(a)
-# make_graph(cluster)
+a = convert2doc(book_dict)
+cluster = create_cluster(a)
+make_graph(cluster)
